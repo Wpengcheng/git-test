4eab2766db2cfafd026f8d7a4721d4dd203ce051
haoming <3194525857@qq.com>
2021-12-17 17:37:37 +0800

    master first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * @User szss
+ * @Classname GitTest
+ * @Project git-test
+ * @Description TODO
+ * @Author wpcheng
+ * @Create 2021-12-17-17:36
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
