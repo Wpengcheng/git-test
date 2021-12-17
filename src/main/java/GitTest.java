704125153fafbeb67c88c01da13d82dfa0a540e0
haoming <3194525857@qq.com>
2021-12-17 17:38:22 +0800

    master third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello");
         System.out.println("git hello2");
+        System.out.println("git hello3");
     }
 }
